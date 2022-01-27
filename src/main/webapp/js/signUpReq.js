2a54d01a0e8d46ba037c24a36f36d3fc70d67439
praveen-pt4650 <spraveeniit@gmail.com>
2022-01-27 17:17:16 +0530

    adds signUp and added ajax call for content load

--- src/main/webapp/js/signUpReq.js
+++ src/main/webapp/js/signUpReq.js
@@ -1,8 +1,64 @@
+// document.querySelector(".signUp").classList.add("invisible");
+// document.querySelector(".sign").addEventListener("click",enable);
+//
+// function enable(){
+//     document.querySelector(".signUp").classList.remove("invisible");
+//
+//     document.querySelector(".sign").classList.add("invisible");
+// }
+//
+//
+//
+$.ajax({
+    type:"GET",
+    url:"content"
+})
 
-document.querySelector(".signUp").style.display = "none";
-document.querySelector(".sign").addEventListener("click",enable);
 
-function enable(){
-    document.querySelector(".signUp").style.display = "block";
-    document.querySelector(".sign").style.display = "none";
-}
+$(".signUp").hide();
+// // document.querySelector(".signUp").style.display = "none";
+// // document.querySelector(".sign").addEventListener("click",enable);
+// $(".sign").click(function (){
+//     $(".signUp").show();
+//     $(this).hide();
+// });
+//
+$("#sign-submit").click(function (){
+    let ajax = $.ajax({
+        type:"POST",
+        url:"sign-up",
+        data: $('form.form-signup').serialize(),
+        dataType:"json",
+        success: function (data){
+            console.log(data);
+            $(".show").text(data);
+        },
+        error: function (data){
+            console.log(data);
+        }
+    });
+    ajax.done(function (data){
+            console.log(JSON.parse(data));
+            $(".show").text(data);
+        });
+    console.log("status");
+});
+
+$(".sample").click(function (event){
+    console.log(event);
+    $.ajax({
+        type:"GET",
+        url: "sample",
+        dataType:"json",
+        success: function (data){
+            console.log(data);
+        },
+        error: function (data){
+            console.log(data);
+        }
+    });
+});
+
+$(".sign").click(function (){
+    $(".signUp").toggle();
+})
