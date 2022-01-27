2e2e119e739ac04a947c92657ca9be4a863394f4
praveen-pt4650 <spraveeniit@gmail.com>
2022-01-27 17:27:24 +0530

    adds writeData class

---
+++ src/main/java/com/praveen/articlemanagement/WriteData.java
@@ -0,0 +1,23 @@
+package com.praveen.articlemanagement;
+
+import java.sql.*;
+
+public class WriteData {
+    public static void signUp(String username,String password) throws Exception{
+        try {
+//            System.out.println(username + " " + password);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/articleManagement", "sample", "sample");
+            String query = "INSERT INTO user (username,password,premium) VALUES (?,?,'NO')";
+            PreparedStatement stmt = con.prepareStatement(query);
+
+            stmt.setString(1,username);
+            stmt.setString(2,password);
+            stmt.execute();
+        }
+        catch (Exception e){
+//            System.out.println(e);
+            throw new Exception();
+        }
+    }
+}
