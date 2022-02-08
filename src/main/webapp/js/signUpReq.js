5e69c135bdafe8f502d006b08d73373588fc7362
praveen-pt4650 <spraveeniit@gmail.com>
2022-02-08 09:31:37 +0530

    modifies js files for ajax login

--- src/main/webapp/js/signUpReq.js
+++ src/main/webapp/js/signUpReq.js
@@ -11,77 +11,85 @@ $.ajax({        //ajax for content-load(latest)
     }
 })
 
+if($("#session-name").val() === ''){
+
+    $(".log-out").hide();
 //sign-up form ajax request (have to take care of password validation
-$("form.form-signup").submit(function (form){
-    form.preventDefault();     //prevent the actual submission of form
-    if ($("#sign-password-1").val() !== $("#sign-password-2").val()){
-        alert("Password Mismatch");
-        return false;
-    }
-    let ajax = $.ajax({
-        type:"POST",
-        url:"sign-up",
-        data: $('form.form-signup').serialize(),
-        dataType:"json",
-        success: function (data){
-            console.log(data);
-            if(data.status === 'success'){
-                alert("Signed Up Successfully!");
-                // $(".sign-up").remove();
-            }
-            else{
-                alert("Username already exists");
-            }
 
-        },
-        error: function (){
-            alert("Error! Try Again!");
+    $("form.form-signup").submit(function (form){
+        form.preventDefault();     //prevent the actual submission of form
+        if ($("#sign-password-1").val() !== $("#sign-password-2").val()){
+            alert("Password Mismatch");
+            return false;
         }
+        let ajax = $.ajax({
+            type:"POST",
+            url:"sign-up",
+            data: $('form.form-signup').serialize(),
+            dataType:"json",
+            success: function (data){
+                console.log(data);
+                if(data.status === 'success'){
+                    alert("Signed Up Successfully!");
+                    // $(".sign-up").remove();
+                }
+                else{
+                    alert("Username already exists");
+                }
+
+            },
+            error: function (){
+                alert("Error! Try Again!");
+            }
+        });
+        ajax.done(function (){
+            $("#form-entry").remove();
+            $(".log-out").show();
+        })
+
     });
-    ajax.done(function (){
-
-    })
-
-});
 
 //login form request
-$("form.form-login").submit(function (form){
-    form.preventDefault();
-    let ajax = $.ajax({
-        type:"GET",
-        url:"log-in",
-        data: $("form.form-login").serialize(),
-        dataType: "json",
-        success: function (data){
-            if(data.status === 'success'){
-                alert("Logged In Successfully!");
-                console.log(data);
-                $(".log-in").remove();
+    $("form.form-login").submit(function (form){
+        form.preventDefault();
+        let ajax = $.ajax({
+            type:"GET",
+            url:"log-in",
+            data: $("form.form-login").serialize(),
+            dataType: "json",
+            success: function (data){
+                if(data.status === 'success'){
+                    alert("Logged In Successfully!");
+                    console.log(data);
+                    $(".log-in").remove();
+                }
+                else{
+                    alert("Incorrect Password/Username");
+                }
             }
-            else{
-                alert("Incorrect Password/Username");
-            }
-        }
+        });
+        ajax.done(function (){
+            $("#form-entry").remove();
+            $(".log-out").show();
+        });
     });
-    ajax.done(function (){
-
-    });
-});
 
 
-$(".log-in").hide();
-$(".sign-up").hide();
+    $(".log-in").hide();
+    $(".sign-up").hide();
 
 //log-in toggle button
 
-$(".log").click(function (){
-    $(".sign-up").hide();
-    $(".log-in").toggle();
-});
+    $(".log").click(function (){
+        $(".sign-up").hide();
+        $(".log-in").toggle();
+    });
 
 //Sign-up toggle button
 
-$(".sign").click(function (){
-    $(".log-in").hide();
-    $(".sign-up").toggle();
-});
+    $(".sign").click(function (){
+        $(".log-in").hide();
+        $(".sign-up").toggle();
+    });
+}
+
